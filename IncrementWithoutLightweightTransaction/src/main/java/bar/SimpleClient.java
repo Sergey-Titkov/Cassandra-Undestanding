f427ad321bc6847d5eae74647a70308aafcbb67e
Sergey.Titkov <Sergey.Titkov@billing.ru>
2014-08-29 16:01:22 +0400

    Многопоточно обновляем счетчики, без использования легковесных тразакций.  Начало разработки.

---
+++ IncrementWithoutLightweightTransaction/src/main/java/bar/SimpleClient.java
@@ -0,0 +1,51 @@
+package bar;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Самый простой вариант подключеня к кластеру.
+ *
+ * @author Sergey.Titkov
+ * @version 001.00
+ * @since 001.00
+ */
+public class SimpleClient {
+  private Logger logger = LoggerFactory.getLogger(getClass());
+
+  private Cluster cluster;
+  private Session session;
+
+  /**
+   * Подключение к кластеру кассандры.
+   * @param node адресс ноды, без адреса!
+   */
+  public void connect(String node) {
+    cluster = Cluster.builder()
+      .addContactPoint(node)
+      .build();
+    session = cluster.connect();
+    Metadata metadata = cluster.getMetadata();
+    logger.debug("Подключились к кластеру: {}", metadata.getClusterName());
+
+    for (Host host : metadata.getAllHosts()) {
+      logger.debug(
+        "Datatacenter: {}; Host: {}; Rack: {}", new Object[]{
+        host.getDatacenter(), host.getDatacenter(), host.getDatacenter()
+      }
+      );
+    }
+  }
+
+  public Session getSession() {
+    return this.session;
+  }
+  public void close() {
+    cluster.close();
+  }
+
+}
