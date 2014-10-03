a1f130eff4fb70313fee528372ef477b07d7588f
Sergey.Titkov <Sergey.Titkov@billing.ru>
2014-10-03 12:24:26 +0400

    Привел в порядок классы. Написал комментарии.

--- CheckClaster/src/main/java/foo/bar/ProcessReadValue.java
+++ CheckClaster/src/main/java/foo/bar/ProcessReadValue.java
@@ -8,13 +8,13 @@ import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * Нить для рандомного обновления баланса.
+ * Нить для чтения данных из таблицы.
  *
  * @author Sergey.Titkov
  * @version 001.00
  * @since 001.00
  */
-public class ProcessReadValue extends Thread implements Comparable<ProcessReadValue> {
+public class ProcessReadValue extends Thread {
   // Наш любимый логер.
   private Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -35,14 +35,19 @@ public class ProcessReadValue extends Thread implements Comparable<ProcessReadVa
   private int numberOfReadTimeoutException = 0;
   private int numberOfErrorReadValue = 0;
   private long numberOfProcessCol = 0;
+  // Сколько прочитали строк за последенее чтение.
   private long numberOfLastProcessCol = 0;
-
+  // Время работы в микросикундах.
   private long duration = 0;
-
   private long sumVol = 0;
 
   /**
    * Основной конструктор
+   *
+   * @param workTime   сколько времени работать в секундах.
+   * @param endWorkCDL На чем ждать завершения работы.
+   * @param readValue  С помощюь чего читать данные.
+   * @param rowKey     Строковый ключ.
    */
   public ProcessReadValue(int workTime, CountDownLatch endWorkCDL, ReadValue readValue, Long rowKey) {
     this.countDownTime = workTime;
@@ -63,7 +68,7 @@ public class ProcessReadValue extends Thread implements Comparable<ProcessReadVa
 
       do {
         ReadValueResult readValueResult = readValue.read(rowKey);
-
+        // Сколько было тайм утов при встаки
         numberOfReadTimeoutException = numberOfReadTimeoutException + readValue.getMaxErrorOccur();
 
         // Увеличиваем счетчик в том случае если совсем не удалось обновить баланс.
@@ -83,48 +88,6 @@ public class ProcessReadValue extends Thread implements Comparable<ProcessReadVa
       this.endWorkCDL.countDown();
       logger.debug("Конец работы нити: {}.", threadUUID);
     }
-  }
-
-  /**
-   * Compares this object with the specified object for order.  Returns a
-   * negative integer, zero, or a positive integer as this object is less
-   * than, equal to, or greater than the specified object.
-   * <p/>
-   * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
-   * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
-   * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
-   * <tt>y.compareTo(x)</tt> throws an exception.)
-   * <p/>
-   * <p>The implementor must also ensure that the relation is transitive:
-   * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
-   * <tt>x.compareTo(z)&gt;0</tt>.
-   * <p/>
-   * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
-   * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
-   * all <tt>z</tt>.
-   * <p/>
-   * <p>It is strongly recommended, but <i>not</i> strictly required that
-   * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
-   * class that implements the <tt>Comparable</tt> interface and violates
-   * this condition should clearly indicate this fact.  The recommended
-   * language is "Note: this class has a natural ordering that is
-   * inconsistent with equals."
-   * <p/>
-   * <p>In the foregoing description, the notation
-   * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
-   * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
-   * <tt>0</tt>, or <tt>1</tt> according to whether the value of
-   * <i>expression</i> is negative, zero or positive.
-   *
-   * @param o the object to be compared.
-   * @return a negative integer, zero, or a positive integer as this object
-   *         is less than, equal to, or greater than the specified object.
-   * @throws NullPointerException if the specified object is null
-   * @throws ClassCastException   if the specified object's type prevents it
-   *                              from being compared to this object.
-   */
-  @Override public int compareTo(ProcessReadValue o) {
-    return Long.compare(duration, o.duration);
   }
 
   public UUID getThreadUUID() {
